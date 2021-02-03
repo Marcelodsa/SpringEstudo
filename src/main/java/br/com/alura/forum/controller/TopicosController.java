89388bc0061ae5d3c6449e5583b7c2e204f4e8e3
MarceloDSA <marcelo.egcomp@gmail.com>
2021-02-03 12:30:06 -0300

    Criando classe dto para passagem dos dados e convertendo de listadto para lista

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +13,8 @@ import java.util.List;
 public class TopicosController {
 
     @RequestMapping("/topicos")
-    public List<Topico> lista(){
+    public List<TopicoDto> lista(){
         Topico topico= new Topico("Duvida", "Duvida com spring", new Curso("Spring", "Programação"));
-        return Arrays.asList(topico, topico, topico);
+        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
     }
 }
