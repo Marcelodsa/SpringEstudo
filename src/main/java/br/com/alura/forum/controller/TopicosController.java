bfec6f534c512b49b0f3eed6a1a558dceca87a67
MarceloDSA <marcelo.egcomp@gmail.com>
2021-02-03 12:02:10 -0300

    Adicionando RestController

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -2,14 +2,15 @@ package br.com.alura.forum.controller;
 
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class TopicosController {
+
     @RequestMapping("/topicos")
     public List<Topico> lista(){
         Topico topico= new Topico("Duvida", "Duvida com spring", new Curso("Spring", "Programação"));
