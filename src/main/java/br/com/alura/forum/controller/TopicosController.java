26ec21869a0d2d07c29fbd27b9b5f7650ef96211
MarceloDSA <marcelo.egcomp@gmail.com>
2021-02-04 11:52:32 -0300

    Consultanto banco com filtros

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -17,8 +16,13 @@ public class TopicosController {
     private TopicoRepository topicoRepository;
 
     @RequestMapping("/topicos")
-    public List<TopicoDto> lista(){
-        List<Topico> topicos= topicoRepository.findAll();
-        return TopicoDto.converter(topicos);
+    public List<TopicoDto> lista(String nomeCurso){
+        if(nomeCurso == null) {
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
+        } else{
+            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+            return TopicoDto.converter(topicos);
+        }
     }
 }
