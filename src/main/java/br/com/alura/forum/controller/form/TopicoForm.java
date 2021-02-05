073721ae26bdca4f24e2c01b332bb765a823b614
Marcelodsa <marcelo.egcomp@gmail.com>
2021-02-05 11:27:16 -0300

    Validando post com Bean Validation e otimizando mensagem de erro com handler

--- src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -3,11 +3,19 @@ package br.com.alura.forum.controller.form;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@SuppressWarnings("unused")
 public class TopicoForm {
 
+    @NotNull @NotEmpty @Length(min = 5)
     private String titulo;
+    @NotNull @NotEmpty @Length(min = 10)
     private String mensagem;
+    @NotNull @NotEmpty
     private String nomeCurso;
 
     public String getTitulo() {
